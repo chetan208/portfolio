6e1494b0d8cbaaa0a21484bd495ade7188134e3d
chetan <chetanchoudhary435@gmail.com>
2025-09-21 22:35:40 +0530

    project card added

---
+++ app.js
@@ -0,0 +1,11 @@
+let gitbutton1=document.querySelector(".gitbutton1");
+let viewdemo1=document.querySelector(".viewdemo1");
+let projectcard1=document.querySelector(".projectcard1");
+
+projectcard1.addEventListener("click" ,()=>{
+   window.open("https://mentisdictionary.netlify.app/","_blank");
+})
+
+gitbutton1.addEventListener("click",()=>{
+    window.open("https://github.com/chetan208/dictionary","_blank");
+})
