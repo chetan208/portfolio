decfa13f9ef518013c2dc6c51f0cc93eed85aa74
chetan <chetanchoudhary435@gmail.com>
2025-09-23 09:47:04 +0530

    bars

--- app.js
+++ app.js
@@ -9,3 +9,26 @@ projectcard1.addEventListener("click" ,()=>{
 gitbutton1.addEventListener("click",()=>{
     window.open("https://github.com/chetan208/dictionary","_blank");
 })
+
+let baricon=document.querySelector(".baricon");
+let hamberger=document.querySelector(".hamberger");
+let header=document.querySelector(".header");
+baricon.addEventListener("click",(e)=>{
+        hamberger.classList.add("active");
+        console.log("added")
+        e.stopPropagation();
+})
+
+
+
+document.addEventListener("click",(e)=>{
+    if(hamberger.classList.contains("active") && !hamberger.contains(e.target)){
+        hamberger.classList.remove("active");
+        console.log("removed");
+    }  
+})
+
+let cross=document.querySelector(".cross");
+cross.addEventListener("click",()=>{
+    hamberger.classList.remove("active");
+})
