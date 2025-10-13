bcd9bda733916a6c5328a877fbf40ac2dbb1100a
chetan <chetanchoudhary435@gmail.com>
2025-10-13 23:53:27 +0530

    hover effect

--- app.js
+++ app.js
@@ -191,3 +191,19 @@ document.querySelectorAll('.skillcard').forEach(card => {
   });
 });
 
+
+document.querySelectorAll('.projectcard').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    card.style.setProperty('--x', `${x}px`);
+    card.style.setProperty('--y', `${y}px`);
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.setProperty('--x', `-9999px`);
+    card.style.setProperty('--y', `-9999px`);
+  });
+});
+
